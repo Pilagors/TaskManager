5f9370c4e8cc2c086dc206dd44dda2bee1d21ea6
Lucas Monfort <Lucas_Monfort@etu.ube.fr>
2026-04-24 11:31:44 +0200

    implémentation usecase de la création d'une tâche

--- app/src/main/java/fr/monfort/taskmanager/domain/usecase/CreateTask.kt
+++ app/src/main/java/fr/monfort/taskmanager/domain/usecase/CreateTask.kt
@@ -1,4 +1,20 @@
 package fr.monfort.taskmanager.domain.usecase
 
-class CreateTask {
+import fr.monfort.taskmanager.data.model.Task
+import fr.monfort.taskmanager.data.repository.TaskRepository
+import java.util.UUID
+
+class CreateTask (
+    private val repository: TaskRepository
+) {
+    fun execute (title: String, parentId: String? = null) {
+        repository.addTask(
+            Task(
+                id = UUID.randomUUID().toString(),
+                title = title,
+                parentId = parentId,
+                order = 0
+            )
+        )
+    }
 }
