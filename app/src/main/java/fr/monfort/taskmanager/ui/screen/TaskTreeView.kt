60ee13e1f65e08e39f787a667a36525ed6cc6e4d
Lucas Monfort <Lucas_Monfort@etu.ube.fr>
2026-04-24 14:02:20 +0200

    Ajustements ui

--- app/src/main/java/fr/monfort/taskmanager/ui/screen/TaskTreeView.kt
+++ app/src/main/java/fr/monfort/taskmanager/ui/screen/TaskTreeView.kt
@@ -1,11 +1,13 @@
 package fr.monfort.taskmanager.ui.screen
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import fr.monfort.taskmanager.data.model.TaskNode
 
@@ -17,9 +19,19 @@ fun TaskTreeView(
     Column() {
         nodes.forEach { node ->
             Card(
-                elevation = CardDefaults.cardElevation(6.dp),
+                elevation = CardDefaults.cardElevation(2.dp),
+                border = if (level == 0) {
+                    BorderStroke(1.dp, Color.Gray)
+                } else {
+                    null
+                },
                 modifier = Modifier
-                .padding(start = (level * 32).dp, top = 4.dp, bottom = 4.dp)
+                .padding(
+                    start = if (level != 0) (level * 32).dp else 16.dp,
+                    top = 4.dp,
+                    bottom = if (level == 0) 16.dp else 4.dp,
+                    end = 16.dp
+                )
             ) {
                 TaskRow(task = node.task)
 
